040ebb1b096460aa25e306a99cae9bdf489ab773
Michael L. Gantz <gantzm@gantzgulch.com>
2022-01-02 20:21:46 -0500

    Added Map.Entry comparators.

--- gantzgulch-tools-common/src/main/java/com/gantzgulch/tools/common/lang/GGMaps.java
+++ gantzgulch-tools-common/src/main/java/com/gantzgulch/tools/common/lang/GGMaps.java
@@ -1,5 +1,6 @@
 package com.gantzgulch.tools.common.lang;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,9 +10,21 @@ public final class GGMaps {
         throw new UnsupportedOperationException();
     }
 
-    public static <K,E> Map<K, E> shallowCopy(final Map<K, E> src) {
+    public static <K, E> Map<K, E> shallowCopy(final Map<K, E> src) {
 
         return src != null ? new HashMap<>(src) : new HashMap<>();
-        
+
+    }
+
+    public static <K extends Comparable<K>, V> Comparator<Map.Entry<K, V>> mapEntryKeyComparator() {
+        return (e1, e2) -> {
+            return GGComparables.compare(e1.getKey(), e2.getKey());
+        };
+    }
+    
+    public static <K, V extends Comparable<V>> Comparator<Map.Entry<K, V>> mapEntryValueComparator() {
+        return (e1, e2) -> {
+            return GGComparables.compare(e1.getValue(), e2.getValue());
+        };
     }
 }
