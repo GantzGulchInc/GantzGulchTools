51ea1b2faaba35a1e3bb2da62a6d47aee6a30ee9
Michael L. Gantz <gantzm@gantzgulch.com>
2022-05-04 11:55:12 -0400

    Adding function to convert raw R & S signature to DER format.

--- gantzgulch-tools-crypto/src/main/java/com/gantzgulch/tools/crypto/alg/ec/ECUtil.java
+++ gantzgulch-tools-crypto/src/main/java/com/gantzgulch/tools/crypto/alg/ec/ECUtil.java
@@ -1,5 +1,6 @@
 package com.gantzgulch.tools.crypto.alg.ec;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Enumeration;
 import java.util.Optional;
 
@@ -8,6 +9,7 @@ import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 import org.bouncycastle.jce.spec.ECParameterSpec;
 
+import com.gantzgulch.logging.core.GGLogger;
 import com.gantzgulch.tools.common.lang.Cast;
 import com.gantzgulch.tools.crypto.BouncyCastleState;
 
@@ -17,6 +19,8 @@ public final class ECUtil {
         BouncyCastleState.init();
     }
 
+    private static final GGLogger LOG = GGLogger.getLogger(ECUtil.class);
+    
     private ECUtil() {
         throw new UnsupportedOperationException();
     }
@@ -44,4 +48,36 @@ public final class ECUtil {
         return Optional.ofNullable(result);
     }
 
+    public static byte[] convertRawSignatureToDer(final byte[] rawRS) {
+        
+        // construct the ASN1Sequence with r and s
+        ByteArrayOutputStream outs = new ByteArrayOutputStream();
+
+        LOG.trace("convertRawSignatureToDer: signed[0]: %x", rawRS[0]);
+        LOG.trace("convertRawSignatureToDer: signed[32]: %x", rawRS[32]);
+
+        byte radd = (byte) (((rawRS[0] & 0x80) > 0) ? 1 : 0);
+        byte sadd = (byte) (((rawRS[32] & 0x80) > 0) ? 1 : 0);
+
+        LOG.trace("convertRawSignatureToDer: radd: %d", radd);
+        LOG.trace("convertRawSignatureToDer: sadd: %d", sadd);
+        
+        byte length = (byte) (0x44 + radd + sadd);
+
+        outs.write(0x30);
+        outs.write(length); // length 68 bytes +
+        outs.write(0x02); // ASN1Integer
+        outs.write(0x20 + radd); // length 32 bytes
+        if (radd > 0)
+            outs.write(0x00); // positive val
+        outs.write(rawRS, 0, 32);
+        outs.write(0x02); // ASN1Integer
+        outs.write(0x20 + sadd); // length 32 bytes
+        if (sadd > 0)
+            outs.write(0x00); // positive val
+        outs.write(rawRS, 32, 32);
+
+        return outs.toByteArray();
+        
+    }
 }
